1c7d60fa467de1f44f38f20c7b737f65d7a3a564
Suhaas D <suhasdgupta55555@gmail.com>
2020-08-03 16:50:44 +0530

    Create ValidPalindrome.java

---
+++ ValidPalindrome.java
@@ -0,0 +1,7 @@
+class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        String actual = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        String rev = new StringBuffer(actual).reverse().toString();
+        return actual.equals(rev);
+    }
+}
