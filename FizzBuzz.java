fb2f43d8d876698f8aa86079318b2fe7bbea6b8d
Suhaas D <suhasdgupta55555@gmail.com>
2020-08-26 20:48:23 +0530

    Create FizzBuzz.java

---
+++ FizzBuzz.java
@@ -0,0 +1,18 @@
+class FizzBuzz {
+    public List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<String>();
+        for(int i=1;i<=n;i++){
+            if(i%3 ==0 && i%5 ==0){
+               list.add("FizzBuzz"); 
+                continue;
+            }
+            if(i%3==0)
+                list.add("Fizz");
+            else if(i%5==0)
+                list.add("Buzz");
+            else
+                list.add(String.valueOf(i));
+        }
+        return list;
+    }
+}
